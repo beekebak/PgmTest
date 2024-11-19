6d3080ee7606b1b67679f1b7494e3abd5f4b204b
b.kovalev <ribehadance@gmail.com>
2024-11-19 08:42:10 +0700

    only core

--- PgmTestExec/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using PgmTest;
-
-string resourcesPath = Path.Combine(AppContext.BaseDirectory, "resources");
-string filePath = Path.Combine(resourcesPath, "Test");
-var checker = new CapturesChecker(filePath);
-checker.Check();
